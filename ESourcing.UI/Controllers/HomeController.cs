56e12b2632680cecc0750bf4180a4f0f45da87be
DESKTOP-CDVJHKO\Hp <azize.aydogdu@dijitalsahne.com>
2021-10-26 16:45:46 +0300

    ESorucing.UI project Layout added and updated.

--- ESourcing.UI/Controllers/HomeController.cs
+++ ESourcing.UI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ESourcing.UI.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace ESourcing.UI.Controllers
@@ -6,6 +7,28 @@ namespace ESourcing.UI.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginModel)
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(AppUserViewModel signupMode)
         {
             return View();
         }
